53d1cb0d73af5d4a23ba1d554926cc892992beef
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-03-07 12:19:45 -0500

    feat(models): Add update_user_posts_counter method to Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,8 @@ class Post < ApplicationRecord
   def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
+
+  def update_user_posts_counter
+    user.update(posts_counter: user.posts.length)
+  end
 end
