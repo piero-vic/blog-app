dea8f41285876ad281e563fd8f42d1752feab727
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-03-07 10:43:32 -0500

    fix(db): Change photo column to be of type text

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_03_211228) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_07_154123) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -46,7 +46,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_03_211228) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.binary "photo"
+    t.text "photo"
     t.text "bio"
     t.integer "posts_counter"
     t.datetime "created_at", null: false
