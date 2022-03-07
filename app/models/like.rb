aad5b40489dd8c4bf8f5045524b7d205e8120cfc
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-03-07 13:46:12 -0500

    fix(models): Update counters after save

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,10 @@ class Like < ApplicationRecord
   belongs_to :post
   belongs_to :user
 
+  after_save :update_post_likes_counter
+
+  private
+
   def update_post_likes_counter
     post.update(likes_counter: post.likes.length)
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,9 +3,13 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  after_save :update_user_posts_counter
+
   def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
+
+  private
 
   def update_user_posts_counter
     user.update(posts_counter: user.posts.length)
