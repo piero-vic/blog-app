dcad75868446297592b18b878b8fd45d79a92667
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-03-10 17:18:00 -0500

    feat(comments): Add functionality to comment in a post

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,23 @@
+class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @current_user = current_user
+    @user_id = params[:user_id]
+    @post_id = params[:post_id]
+    comment = Comment.new(comment_params.merge(author_id: @current_user.id, post_id: @post_id))
+    if comment.save
+      redirect_to user_post_path(@user_id, @post_id), success: 'Comment published'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+end
