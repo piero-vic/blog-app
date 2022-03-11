3df02862f255cced291c6017f12bb28a4688a432
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-03-11 17:58:14 -0500

    test(models): Add test for models

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  before do
+    User.create(id: 1, name: 'User 1', posts_counter: 0)
+    Post.create(id: 1, author_id: 1, title: 'Hello 1', text: 'This is my first post', comments_counter: 0,
+                likes_counter: 0)
+  end
+
+  it 'should update the post comments_counter when created' do
+    Comment.create(post_id: 1, author_id: 1, text: 'This is my first comment')
+    expect(Post.take.comments_counter).to eq 1
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  subject { User.new(id: 1, name: 'User 1', posts_counter: 0) }
+
+  before { subject.save }
+
+  it 'should have name' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'should have posts_counter' do
+    subject.posts_counter = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'posts_counter should be an integer' do
+    subject.posts_counter = 1.1
+    expect(subject).to_not be_valid
+  end
+
+  it 'posts_counter should be greater or equal to 0' do
+    subject.posts_counter = -1
+    expect(subject).to_not be_valid
+  end
+
+  describe '#recent_posts' do
+    it 'should return 3 posts' do
+      Post.create(author_id: 1, title: 'Hello 1', text: 'This is my first post', comments_counter: 0, likes_counter: 0)
+      Post.create(author_id: 1, title: 'Hello 2', text: 'This is my second post', comments_counter: 0, likes_counter: 0)
+      Post.create(author_id: 1, title: 'Hello 3', text: 'This is my third post', comments_counter: 0, likes_counter: 0)
+      Post.create(author_id: 1, title: 'Hello 4', text: 'This is my fourth post', comments_counter: 0, likes_counter: 0)
+      Post.create(author_id: 1, title: 'Hello 5', text: 'This is my fifth post', comments_counter: 0, likes_counter: 0)
+      expect(subject.recent_posts.length).to eq 3
+    end
+  end
+end
