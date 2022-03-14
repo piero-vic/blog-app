f17aa859984151438fbe633245cf294728efff4e
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-03-14 15:38:46 -0500

    feat: Set up devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,18 @@ class ApplicationController < ActionController::Base
     @current_user = current_user
   end
 
-  def current_user
-    User.take
+  # def current_user
+  #   User.take
+  # end
+
+  protect_from_forgery with: :exception
+
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :posts_counter) }
+    # devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :surname, :email, :password, :current_password)}
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+
   def index
     @current_user = current_user
     @users = User.all.order(created_at: :asc)
