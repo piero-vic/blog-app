3850f5822addc3251a63759e17e461128ee99159
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-03-14 17:33:00 -0500

    feat: Add log out button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,6 @@ class ApplicationController < ActionController::Base
     @current_user = current_user
   end
 
-  # def current_user
-  #   User.take
-  # end
-
   protect_from_forgery with: :exception
 
   before_action :update_allowed_parameters, if: :devise_controller?
@@ -14,7 +10,17 @@ class ApplicationController < ActionController::Base
   protected
 
   def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :posts_counter) }
-    # devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :surname, :email, :password, :current_password)}
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:name, :email, :password, :posts_counter)
+    end
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :bio, :email, :password, :current_password)
+    end
+  end
+
+  private
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path if resource_or_scope == :user
   end
 end
