3c4be1d0340c62edf448aa4769f8a3c1a189ae3a
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-03-17 15:36:53 -0500

    chore: Update database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,22 @@
-# Users
-first_user = User.create(name: 'Tom', photo: 'https://randomuser.me/api/portraits/men/75.jpg', bio: 'Teacher from Mexico.', posts_counter: 0)
-second_user = User.create(name: 'Lilly', photo: 'https://randomuser.me/api/portraits/women/75.jpg', bio: 'Teacher from Poland.', posts_counter: 0)
+User.create(id: 1, name: 'Qwerty', email: 'qwerty@qwerty.com', bio: "Hello there! I'm Qwerty.",
+            photo: 'https://randomuser.me/api/portraits/men/75.jpg',
+            password: 'qweqwe123', posts_counter: 0)
 
-# Posts
-first_post = Post.create(author_id: first_user.id, title: 'Hello', text: 'This is my first post', comments_counter: 0, likes_counter: 0)
-second_post = Post.create(author_id: first_user.id, title: 'Hello 2', text: 'This is my second post', comments_counter: 0, likes_counter: 0)
-third_post = Post.create(author_id: first_user.id, title: 'Hello 3', text: 'This is my third post', comments_counter: 0, likes_counter: 0)
-fourth_post = Post.create(author_id: first_user.id, title: 'Hello 4', text: 'This is my fourth post', comments_counter: 0, likes_counter: 0)
+User.create(id: 2, name: 'Ytrewq', email: 'ytrewq@ytrewq.com', bio: "Hello there! I'm Ytrewq.",
+            photo: 'https://randomuser.me/api/portraits/men/55.jpg',
+            password: 'ewqewq321', posts_counter: 0)
 
-# Comments
-first_comment = Comment.create(post_id: first_post.id, author_id: second_user.id, text: 'Hi Tom!' )
-second_comment = Comment.create(post_id: first_post.id, author_id: second_user.id, text: 'Hello Tom!' )
-third_comment = Comment.create(post_id: first_post.id, author_id: second_user.id, text: 'Nice Post Tom!' )
-fourth_comment = Comment.create(post_id: first_post.id, author_id: second_user.id, text: 'Hola Tom!' )
-fifth_comment = Comment.create(post_id: first_post.id, author_id: second_user.id, text: 'Ciao Tom!' )
-sixth_comment = Comment.create(post_id: first_post.id, author_id: second_user.id, text: 'Hallo Tom!' )
+Post.create(id: 1, author_id: 1, title: 'Hello 1', text: 'This is my first post', comments_counter: 0,
+            likes_counter: 0)
+
+Post.create(id: 2, author_id: 1, title: 'Hello 2', text: 'This is my second post', comments_counter: 0,
+            likes_counter: 0)
+
+Post.create(id: 3, author_id: 1, title: 'Hello 3', text: 'This is my third post', comments_counter: 0,
+            likes_counter: 0)
+
+Post.create(id: 4, author_id: 1, title: 'Hello 4', text: 'This is my fourth post', comments_counter: 0,
+            likes_counter: 0)
+
+Comment.create(author_id: 1, post_id: 2, text: 'This is my first comment')
+Like.create(author_id: 1, post_id: 1)
