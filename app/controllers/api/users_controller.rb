3995798a860d9ab11c67d058d10c5041499f590e
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-03-18 22:37:10 -0400

    Configure api logic for user controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -8,4 +8,23 @@ class Api::UsersController < ApplicationController
   def show
     render json: User.find(params[:id])
   end
+
+  def create
+    user = User.create!(user_params)
+    auth_token = AuthenticateUser.new(user.email, user.password).call
+    response = { message: 'Account created successfully', auth_token: auth_token }
+    json_response(response, :created)
+  end
+
+  private
+
+  def user_params
+    params.permit(
+      :name,
+      :email,
+      :bio,
+      :photo,
+      :password
+    )
+  end
 end
